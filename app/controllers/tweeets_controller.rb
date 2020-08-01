11c1ed30dc787a4d46b71e38cdc724c49cd49d18
Uduak Essien <acushla4real@gmail.com>
2020-08-01 07:43:50 +0100

    update tweeet table to reference user, show delete icon for only user own tweets

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -17,7 +17,7 @@ class TweeetsController < ApplicationController
 
   # GET /tweeets/new
   def new
-    @tweeet = Tweeet.new
+    @tweeet = current_user.tweeets.build
   end
 
   # GET /tweeets/1/edit
@@ -27,7 +27,7 @@ class TweeetsController < ApplicationController
   # POST /tweeets
   # POST /tweeets.json
   def create
-    @tweeet = Tweeet.new(tweeet_params)
+    @tweeet = current_user.tweeets.build(tweeet_params)
 
     respond_to do |format|
       if @tweeet.save
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_122856) do
+ActiveRecord::Schema.define(version: 2020_08_01_060030) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
