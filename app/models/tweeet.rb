49f91dfb0cb0e06f39d398944d5bb3c52995a1d5
Uduak Essien <acushla4real@gmail.com>
2020-08-01 14:01:31 +0100

    add validations to user and tweeet models

--- app/models/tweeet.rb
+++ app/models/tweeet.rb
@@ -1,3 +1,6 @@
 class Tweeet < ApplicationRecord
   belongs_to :user
+
+  validates :tweeet, :user, presence: true
+  validates :tweeet, length: { maximum: 180 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :username, :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :email, presence: true, uniqueness: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+  validates :password, presence: true, length: { in: 6..20 }
 end
